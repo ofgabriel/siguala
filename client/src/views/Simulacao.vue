8207982a7b044c26fd8426001330726c2a97ba6f
Gabriel Oliveira <gabriel.oliveira@poli.ufrj.br>
2021-05-05 19:18:40 -0300

    Adiciona view para simulação.

---
+++ client/src/views/Simulacao.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <h1 class="text-center">
+      Página para a simulaçao do Selo
+    </h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  titulo: 'Selo Diversidade - Simulação',
+};
+</script>
