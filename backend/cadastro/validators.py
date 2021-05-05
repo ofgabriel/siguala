f01907a557d3f8fa8c94690aa946614ca92dbbbc
Leonardo Solé Rodrigues <leosole@poli.ufrj.br>
2021-05-05 16:52:24 -0300

    Add validação cnpj

--- backend/cadastro/validators.py
+++ backend/cadastro/validators.py
@@ -2,10 +2,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 def digito_verificador(digitos, posicao):
-  if posicao == 2:
-    pesos = [5,6,7,8,9,2,3,4,5,6,7,8,9,0]
-  else:
-    pesos = [6,7,8,9,2,3,4,5,6,7,8,9,0,0]
+  pesos = [5,6,7,8,9,2,3,4,5,6,7,8,9,0] if posicao==2 else [6,7,8,9,2,3,4,5,6,7,8,9,0,0]
   soma = 0
   for d, p in zip(digitos, pesos):
     soma += d*p
@@ -18,8 +15,6 @@ def validar_cnpj(cnpj):
       if digitos[12] == digito_verificador(digitos, 1):
         if digitos[13] == digito_verificador(digitos, 2):
           return True
-
-  cnpj = digito_verificador(digitos, 1)
   raise ValidationError(
     _('%(cnpj)s não é um CNPJ válido'),
     params={'cnpj': cnpj},
