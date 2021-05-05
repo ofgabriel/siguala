0695fcd6bfaa6078781aea373b67b9b939379c16
Leonardo Solé Rodrigues <leosole@poli.ufrj.br>
2021-05-05 16:48:32 -0300

    Add validação cnpj

--- backend/cadastro/models.py
+++ backend/cadastro/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from .validators import *
 
 # Create your models here.
 class Empresa(models.Model):
     nome = models.CharField(max_length=255)
-    cnpj = models.CharField(max_length=12, verbose_name='CNPJ')
+    cnpj = models.CharField(max_length=14, verbose_name='CNPJ', validators=[validar_cnpj])
     email = models.EmailField(verbose_name='E-mail')
 
     def __str__(self):
