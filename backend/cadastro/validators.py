dd0732292a752d91e16021c299de7b309bea3136
Leonardo Solé Rodrigues <leosole@poli.ufrj.br>
2021-05-16 21:00:47 +0000

    Endpoint para persistência de informações básicas

---
+++ backend/cadastro/validators.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def digito_verificador(digitos, posicao):
+  pesos = [5,6,7,8,9,2,3,4,5,6,7,8,9,0] if posicao==2 else [6,7,8,9,2,3,4,5,6,7,8,9,0,0]
+  soma = 0
+  for d, p in zip(digitos, pesos):
+    soma += d*p
+  return soma % 11
+
+def validar_cnpj(cnpj):
+  if len(cnpj) == 14:
+    if cnpj.isnumeric():
+      digitos = [int(d) for d in cnpj]
+      if digitos[12] == digito_verificador(digitos, 1):
+        if digitos[13] == digito_verificador(digitos, 2):
+          return True
+  raise ValidationError(
+    _('%(cnpj)s não é um CNPJ válido'),
+    params={'cnpj': cnpj},
+  )
+
+
