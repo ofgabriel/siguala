61c16b24a73d0381c1b5ed3d32ae62636f98d6d5
leosole <8992517+leosole@users.noreply.github.com>
2021-05-31 08:12:35 -0300

    Remove linhas comentadas

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -55,10 +55,9 @@ MIDDLEWARE = [
 
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8080",
-# ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
 
 ROOT_URLCONF = 'app.urls'
 
--- client/src/api.js
+++ client/src/api.js
@@ -4,9 +4,5 @@ export default axios.create({
   baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
-    // 'Authorization': "JWT " + localStorage.getItem('token')
   },
-  // xsrfCookieName: 'csrftoken',
-  // xsrfHeaderName: 'X-CSRFToken',
-  // withCredentials: true
 });
