7b4f6268990db69ad8ac4800a7db39992f63c854
leosole <8992517+leosole@users.noreply.github.com>
2021-06-02 20:22:53 -0300

    Adiciona logos

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,8 +14,8 @@
       <router-link :to="{ name: 'Home' }">
         <v-toolbar-title class="white--text pr-10 menu">
           <span
-            class="logo"
             v-if="$vuetify.breakpoint.width > 700"
+            class="logo"
           /> Selo de Igualdade de Gênero
         </v-toolbar-title>
       </router-link>
@@ -64,40 +64,48 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
     <footer
       id="footer"
     >
-      <p class="footer-txt">
-        Este projeto é uma iniciativa de professoras e alunos da Faculdade de Direito da UFRJ,
-        membros da Liga de Direito e Tecnologia, e desenvolvido em parceria com alunos da
-        COPPE/UFRJ (Instituto Alberto Luiz Coimbra de Pós-Graduação e Pesquisa de Engenharia).
-      </p>
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="4">
+          <p class="footer-txt">
+            Este projeto é uma iniciativa de professoras e alunos da Faculdade de Direito da UFRJ,
+            membros da Liga de Direito e Tecnologia, e desenvolvido em parceria com alunos da
+            COPPE/UFRJ (Instituto Alberto Luiz Coimbra de Pós-Graduação e Pesquisa de Engenharia).
+          </p>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-img :src="logo_fnd" class="img-footer" max-width="160px" contain />
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-img :src="logo_coppe" class="img-footer" max-width="150px" contain />
+        </v-col>
+      </v-row>
     </footer>
   </v-app>
 </template>
 
 <script>
+import logoCoppe from '../public/logo-coppe.svg';
+import logoFND from '../public/logo-fnd.png';
 
 export default {
-  components: {},
   name: 'App',
+  components: {},
 
   data: () => ({
     drawer: false,
     group: null,
+    logo_fnd: logoFND,
+    logo_coppe: logoCoppe,
   }),
 };
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap');
-/* body, #app, .container, .v-main__wrap, .main {
-  display: flex !important;
-  min-height: 100vh !important;
-  flex-direction: column !important;
-  flex:1;
-} */
 .logo {
   height: 36px;
   width: 36px;
@@ -123,5 +131,8 @@ export default {
 .main {
   width:100% !important;
   max-width:100% !important;
+}
+.img-footer {
+  margin: auto;
 }
 </style>
