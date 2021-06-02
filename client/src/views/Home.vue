a2692cfbdda676e14678728aa770d650c2d8bd36
leosole <8992517+leosole@users.noreply.github.com>
2021-06-02 20:22:30 -0300

    Adiciona logos

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-container class="ml-0 mr-0 pa-0 main" fluid>
+  <v-container class="ml-0 mr-0 pa-0 main central">
     <informacoes-basicas />
-    <explicacao-funcionamento />
+    <v-coontainer class="ml-auto mr-auto explicacoes">
+      <explicacao-funcionamento />
+      <explicacao-etapas />
+    </v-coontainer>
   </v-container>
 </template>
 
 <script>
+import ExplicacaoEtapas from '../components/ExplicacaoEtapas.vue';
 import ExplicacaoFuncionamento from '../components/ExplicacaoFuncionamento.vue';
 import InformacoesBasicas from '../components/InformacoesBasicas.vue';
 
@@ -16,6 +20,16 @@ export default {
   components: {
     InformacoesBasicas,
     ExplicacaoFuncionamento,
+    ExplicacaoEtapas,
   },
 };
 </script>
+<style>
+  .explicacoes {
+    display: inline-block;
+    max-width: 1000px;
+  }
+  .central {
+    text-align: center
+  }
+</style>
