8af96564fe893de1d2b28bacbfd526035f5cc6d1
Mario Gunter Simao <mariosimao@poli.ufrj.br>
2021-06-06 23:19:14 -0300

    Separar URL da api em nova variavel

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.VUE_APP_API_HOST,
   headers: {
     'Content-Type': 'application/json',
   },
